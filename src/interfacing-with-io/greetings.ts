afb6705572b4b3f34023133944b89aa7be40b8d6
Wassim Mansouri <wassim.mansouri@gmail.com>
2023-08-19 19:36:38 +0200

    Working with IO

--- src/interfacing-with-io/greetings.ts
+++ src/interfacing-with-io/greetings.ts
@@ -1,4 +1,13 @@
-// Listen for user input
+// Console APIs are using process.STDIN, process.STDOUT or process.STDERR under the hood
+// Listen for user input via STDIN
 process.stdin.on("data", (data) => {
-  console.log(data);
+  const name = data.toString().trim().toUpperCase();
+
+  if (name !== "") {
+    // Use STDOUT
+    process.stdout.write(`Hello ${name}`);
+  } else {
+    // Use STDERR
+    process.stderr.write("Input was empty");
+  }
 });
